02c79a11edec658392a4300f52ead9967a88dd7a
hast0n <devreese.martin@gmail.com>
2020-02-17 21:46:42 +0100

    first upload

---
+++ CliLayoutRenderTools/CliLayoutRenderTools/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CliLayoutRenderTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rd = new Renderer()
+            {
+                FrameWidth = 70
+            };
+
+            rd.AddMultipleVisualResources(new Dictionary<string, string>()
+            {
+                {
+                    "intro",
+                    "Bienvenue ! C'est cool walah"
+                },
+            });
+
+            string test = rd.GetResourceRepr("5*[intro]");
+
+            var welcomeScreen = new ScreenTemplate();
+
+        }
+    }
+}
